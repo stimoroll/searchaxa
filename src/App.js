e38e269286fa29f361bd663d1f02a13f5ec66f11
Krzysztof Popławski <stimoroll@gmail.com>
2021-10-11 12:52:30 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Search from "./Search/Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Search filter - just start typing IS...</h2>
+      <Search />
+    </div>
+  );
+}
